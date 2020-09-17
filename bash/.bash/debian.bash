9957f93ddccf6bd2815d0662bdca61581e06a54b
Hubert LOBIT <hubert.lobit@allocloud.com>
2020-09-17 14:07:05 +0200

    Alias `fd` to `fdfind` on debian

--- bash/.bash/debian.bash
+++ bash/.bash/debian.bash
@@ -1,3 +1,7 @@
 DEBEMAIL="hlo@escaux.com"
 DEBFULLNAME="Hubert Lobit"
 export DEBEMAIL DEBFULLNAME
+
+# fd is fdfind !
+alias fd=fdfind
+export FZF_DEFAULT_COMMAND='fdfind --type f --hidden --follow --exclude .git --color=always'
