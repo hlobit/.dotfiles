ccc6f052f9bbf4b5b0a3d7cc51e0248184b362a6
Hubert LOBIT <hubert.lobit@allocloud.com>
2020-09-17 14:36:43 +0200

    Use `gfd` to have a full diff with untracked files

--- bash/.bash/git.bash
+++ bash/.bash/git.bash
@@ -10,3 +10,5 @@ alias gr='cd "$(git rev-parse --show-toplevel)"'
 
 # https://coderwall.com/p/wgq89a/use-tree-with-gitignore
 alias gt='tree -a --prune -I .git -I "$(cat .gitignore ~/.gitignore_global 2>/dev/null | egrep -v "^#.*$|^[[:space:]]*$" | tr "\\n" "|").git"'
+
+alias gfd='(git --no-pager diff && for next in $(git ls-files --others --exclude-standard) ; do git --no-pager diff --no-index /dev/null "$next"; done)'
