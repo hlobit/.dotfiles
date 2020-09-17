5b1d492fc726d78d1bfeb163a816d5903b5c5ce9
Hubert LOBIT <hubert.lobit@allocloud.com>
2020-09-17 14:35:49 +0200

    Use `gt` to have a tree view of git repository

--- bash/.bash/git.bash
+++ bash/.bash/git.bash
@@ -7,3 +7,6 @@ alias gd='git diff'
 alias gdc='git diff --cached'
 alias gh='git show HEAD'
 alias gr='cd "$(git rev-parse --show-toplevel)"'
+
+# https://coderwall.com/p/wgq89a/use-tree-with-gitignore
+alias gt='tree -a --prune -I .git -I "$(cat .gitignore ~/.gitignore_global 2>/dev/null | egrep -v "^#.*$|^[[:space:]]*$" | tr "\\n" "|").git"'
