a17ab14284482c87bd390ed6d577118cfce3b1fb
Hubert LOBIT <hubert.lobit@allocloud.com>
2020-09-23 14:06:50 +0200

    Attempt to get an eternal bash history

--- bash/.bash/main.bash
+++ bash/.bash/main.bash
@@ -3,3 +3,10 @@ export VISUAL="$EDITOR"
 
 bind 'set enable-bracketed-paste on'
 bind 'set revert-all-at-newline on'
+
+# eternal bash history
+export HISTSIZE=
+export HISTFILESIZE=
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
