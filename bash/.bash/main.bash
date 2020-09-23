1291582f2a88813d332b098afe19548574caafef
Hubert LOBIT <hubert.lobit@allocloud.com>
2020-09-23 14:07:54 +0200

    Unify bash history when different windows/panes

--- bash/.bash/main.bash
+++ bash/.bash/main.bash
@@ -10,3 +10,11 @@ export HISTFILESIZE=
 # Change the file location because certain bash sessions truncate .bash_history file upon close.
 # http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
 export HISTFILE=~/.bash_eternal_history
+
+# NOTE: maybe we should just log instead of using unified bash history
+# this way we would not merge contexts across terminals while keeping access to the entire history
+#
+# https://superuser.com/a/734410
+# unified bash history
+shopt -s histappend
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
